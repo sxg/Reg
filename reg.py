3fee04227cac67d0300134ac256eaa5ee02e0f8d
Satyam Ghodasara <sghodas@gmail.com>
2018-10-24 22:11:44 -0500

    Added output path and saving of registered images

--- reg.py
+++ reg.py
@@ -14,10 +14,12 @@ import hdf5storage
 @click.argument("path")
 @click.option("-f", "--fnirt-path", default="/usr/local/fsl/bin/fnirt",
               help="Path to FSL's FNIRT executable. Defaults to /usr/local/fsl/bin/fnirt.")
+@click.option("-o", "--output-path", default="./",
+              help="Output path for registered images. Defaults to current path.")
 @click.option("-n", "--name", required=True, help="Image dataset name.")
 @click.option("-a", "--anchors", default="1",
               help="Comma-separated list of anchor frames. Defaults to the first frame.")
-def cli(path, fnirt_path, name, anchors):
+def cli(path, fnirt_path, output_path, name, anchors):
     """CLI that prints "Hello, world!"""
 
     # Parse and convert anchor indexes to Python indexes
@@ -41,6 +43,9 @@ def cli(path, fnirt_path, name, anchors):
         reg_data(fnirt_path, tmp_path, anchors, img.shape[3])
         # Load the registered data
         reg_img = load_reg_vols(tmp_path, anchors, img.shape)
+        # Save the registered data
+        hdf5storage.savemat(os.path.join(output_path, "registeredImages.mat"),
+                            {"registeredImages": reg_img})
         input("Press <Enter> to continue...")
 
 def reg_data(fnirt_path, tmp_path, anchors, n_vols):
